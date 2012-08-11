591a0f82b68f67f7cb79aa9681191e9f37ca5bc8
Mina Nagy Zaki <mnzaki@gmail.com>
2012-08-11 12:45:18 +0200

    Remove old code

--- lib/rubber/commands/config.rb
+++ lib/rubber/commands/config.rb
@@ -67,43 +67,9 @@ module Rubber
           cfg.instance.add(instance)
           gen = Rubber::Configuration::Generator.new("#{Rubber.root}/config/rubber", role_names, instance_alias)
           gen.fake_root ="#{Rubber.root}/tmp/rubber"
-        elsif Rubber::Configuration.rubber_env.discover_instances
-          # Discover ourselves.
-          # FIXME this is very AWS specific
-          begin
-            require 'open-uri'
-            Timeout::timeout(5) do
-              instance_id = URI.parse('http://169.254.169.254/latest/meta-data/instance-id').read
-            end
-            server = cloud.describe_instances(instance_id).first
-            instance_roles = server.tags['Roles'].split('|').collect do |role_value|
-              RoleItem.parse(role_value)
-            end
-            instance_alias = server.tags['Name']
-            instance = Rubber::Configuration::InstanceItem.new(
-                instance_alias,
-                server.tags['Domain'],
-                instance_roles,
-                server.attributes[:id],
-                server.attributes[:flavor_id],
-                server.attributes[:image_id],
-                server.attributes[:groups].reject {|sg| sg.match(/^sg/)})
-            instance.external_host = server.attributes[:dns_name]
-            instance.external_ip = server.attributes[:public_ip_address]
-            instance.internal_host = server.attributes[:private_dns_name]
-            instance.internal_ip = server.attributes[:private_ip_address]
-            instance.zone = server.attributes[:availability_zone]
-            instance.platform = server.attributes[:platform]
-            instance.root_device_type = server.attributes[:root_device_type]
-
-            cfg.instance.add(instance)
-            env = cfg.environment.bind(role_names, instance_alias)
-            gen = Rubber::Configuration::Generator.new(
-              "#{Rubber.root}/config/rubber", server.tags['Roles'].gsub("|", ","), instance_alias)
-          rescue
-            puts "Instance not found for host: #{instance_alias}"
-            exit 1
-          end
+        else
+          puts "Instance not found for host: #{instance_alias}"
+          exit 1
         end
 
         if file
