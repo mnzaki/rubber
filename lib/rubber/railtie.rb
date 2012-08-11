9b0f1e5d6172d43f8ad7ea2735b2dd8b55862454
Mina Nagy Zaki <mnzaki@gmail.com>
2012-08-11 15:35:43 +0200

    Only load rubber in rails if on production.

--- lib/rubber/railtie.rb
+++ lib/rubber/railtie.rb
@@ -6,7 +6,7 @@ module Rubber
   class Railtie < Rails::Railtie
 
     config.before_configuration do
-      Rubber::initialize(Rails.root, Rails.env)
+      Rubber::initialize(Rails.root, Rails.env) if Rails.env == "production"
     end
 
   end
